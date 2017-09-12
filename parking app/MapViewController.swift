39f9cc7506a7cc3a0631260bb1f7619ecb6c595d
Farbod909 <rafezyfarbod@gmail.com>
2017-09-12 00:17:14 -0700

    Implement translucent drawer similar to Apple Maps

---
+++ parking app/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  parking app
+//
+//  Created by Farbod Rafezy on 9/12/17.
+//  Copyright © 2017 Farbod Rafezy. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
