d6aa24cf96600f852ea04a19d0c0bf26b523f342
Farbod909 <rafezyfarbod@gmail.com>
2017-09-12 14:36:06 -0700

    Show current location on app open

--- parking app/MapViewController.swift
+++ parking app/MapViewController.swift
@@ -8,14 +8,38 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
+    private let locationManager = LocationManager.shared
+    var loadedInitialCurrentLocation = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.showsUserLocation = true
+
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLDistanceFilterNone;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !loadedInitialCurrentLocation {
+            centerMapOnLocation(location: locations[0])
+            loadedInitialCurrentLocation = true
+        }
+    }
+
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
 }
