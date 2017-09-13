687301fed5d7728dd93d091730340d1f341f5269
Farbod909 <rafezyfarbod@gmail.com>
2017-09-13 02:20:15 -0700

    Add logo placeholder

--- parking app/MapViewController.swift
+++ parking app/MapViewController.swift
@@ -12,6 +12,8 @@ import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var logoText: UILabel!
     @IBOutlet weak var mapView: MKMapView!
 
     private let locationManager = LocationManager.shared
@@ -25,6 +27,24 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.distanceFilter = kCLDistanceFilterNone;
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         locationManager.requestLocation()
+
+        logoText.textColor = UIColor.gray.withAlphaComponent(0.8)
+
+        logoView.layer.cornerRadius = 10
+        logoView.layer.masksToBounds = true
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            logoView.backgroundColor = UIColor.clear
+
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.prominent)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+
+            blurEffectView.frame = logoView.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+            logoView.addSubview(blurEffectView)
+        } else {
+            self.view.backgroundColor = UIColor.white
+        }
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
