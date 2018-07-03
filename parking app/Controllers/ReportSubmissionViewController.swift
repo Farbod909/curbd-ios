6c28c998eebfd249cfb91bc256d33db2bd21aa2f
Farbod909 <rafezyfarbod@gmail.com>
2018-07-02 22:16:30 -0700

    Create dynamic report page

---
+++ parking app/Controllers/ReportSubmissionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ReportSubmissionViewController.swift
+//  parking app
+//
+//  Created by Farbod Rafezy on 7/2/18.
+//  Copyright © 2018 Farbod Rafezy. All rights reserved.
+//
+
+import UIKit
+
+class ReportSubmissionViewController: UIViewController {
+
+    var reportReason: String?
+    @IBOutlet weak var commentsTextView: UITextView!
+    @
+    IBAction func submitButtonClick(_ sender: UIBarButtonItem) {
+        if let reportReason = reportReason {
+            // submit report
+        }
+    }
+    
+}
