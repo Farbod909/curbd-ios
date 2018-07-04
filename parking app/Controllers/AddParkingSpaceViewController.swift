e03f62f8f95783ffd244d673d15f868be3eed295
Farbod909 <rafezyfarbod@gmail.com>
2018-07-04 00:49:48 -0700

    Add beginner form elements to add listing page

--- parking app/Controllers/AddParkingSpaceViewController.swift
+++ parking app/Controllers/AddParkingSpaceViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import Eureka
+import MapKit
 
 class AddParkingSpaceViewController: FormViewController {
+
     func initializeSettings() {
         animateScroll = true
 
@@ -24,7 +26,71 @@ class AddParkingSpaceViewController: FormViewController {
     }
 
     func initializeForm() {
-        
+
+        form
+            +++ Section("Parking Space Address")
+            <<< TextRow("street address") {
+                $0.placeholder = $0.tag?.capitalized
+            }
+
+            <<< TextRow("unit number") {
+                $0.placeholder = "Unit #"
+            }
+
+            <<< ZipCodeRow("zip code") {
+                $0.placeholder = $0.tag?.capitalized
+            }.onChange() { zipCodeRow in
+                if let zipCode = zipCodeRow.value, zipCode.count >= 5 {
+                    if  let cityRow = self.form.rowBy(tag: "city") as? TextRow,
+                        let stateRow = self.form.rowBy(tag: "state") as? TextRow {
+                        let geocoder = CLGeocoder()
+                        geocoder.geocodeAddressString(zipCode) { placemarks, error in
+                            if(error != nil) {
+                                print("Error", error ?? "")
+                            }
+                            if let placemark = placemarks?.first {
+                                cityRow.value = placemark.locality
+                                stateRow.value = placemark.administrativeArea
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            <<< TextRow("city") {
+                $0.placeholder = $0.tag?.capitalized
+//                $0.disabled = Condition.function(["zip code"], { form in
+//                    if let zipCodeRow = form.rowBy(tag: "zip code") as? ZipCodeRow {
+//                        return zipCodeRow.value == nil
+//                    }
+//                    return true
+//                })
+            }
+
+            <<< TextRow("state") {
+                $0.placeholder = $0.tag?.capitalized
+//                $0.disabled = Condition.function(["zip code"], { form in
+//                    if let zipCodeRow = form.rowBy(tag: "zip code") as? ZipCodeRow {
+//                        return zipCodeRow.value == nil
+//                    }
+//                    return true
+//                })
+            }
+
+            +++ IntRow("available spots") {
+                $0.title = $0.tag?.capitalized
+                $0.value = 1
+                }.onChange() {
+                    if $0.value == nil {
+                        $0.value = 1
+                    }
+            }
+
+            <<< MultipleSelectorRow<String>("features") {
+                $0.title = $0.tag?.capitalized
+                $0.options = ["Covered", "Charging", "Guarded", "Surveillance", "Illuminated"]
+            }
     }
 
     @objc func nextButtonClick(sender: UIBarButtonItem) {
