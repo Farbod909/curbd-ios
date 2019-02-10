139b9aad58140752530a51c1744fb92dab21f23a
Farbod909 <rafezyfarbod@gmail.com>
2019-02-09 22:28:06 -0800

    Check password minimum length is 8

--- Curbd/Controllers/SignupPasswordViewController.swift
+++ Curbd/Controllers/SignupPasswordViewController.swift
@@ -34,55 +34,61 @@ class SignupPasswordViewController: UIViewController {
     }
     
     @IBAction func submitButtonClick(_ sender: UIButton) {
-        if passwordTextField.text != "" && confirmPasswordTextField.text != "" {
-            if passwordTextField.text == confirmPasswordTextField.text {
-                if  let firstName = firstName?.trim(),
-                    let lastName = lastName?.trim(),
-                    let email = email?.trim(),
-                    let phone = phone?.trim(),
-                    let password = passwordTextField.text {
+        if passwordTextField.text == confirmPasswordTextField.text {
+            if passwordTextField.text != "" {
+                if let passwordLength = passwordTextField.text?.count, passwordLength >= 8 {
+                    if  let firstName = firstName?.trim(),
+                        let lastName = lastName?.trim(),
+                        let email = email?.trim(),
+                        let phone = phone?.trim(),
+                        let password = passwordTextField.text {
 
-                    startLoading(loadingView)
-                    User.create(
-                        firstName: firstName,
-                        lastName: lastName,
-                        email: email,
-                        phone: phone,
-                        password: password) { error, user in
-                            self.stopLoading(self.loadingView)
+                        startLoading(loadingView)
+                        User.create(
+                            firstName: firstName,
+                            lastName: lastName,
+                            email: email,
+                            phone: phone,
+                            password: password) { error, user in
+                                self.stopLoading(self.loadingView)
 
-                            if error != nil {
-                                if let error = error as? ValidationError {
-                                    self.presentValidationErrorAlert(from: error)
-                                } else {
-                                    self.presentServerErrorAlert()
-                                }
-                            } else {
-                                User.login(username: email, password: password) { error in
-                                    if error != nil {
-                                        self.presentServerErrorAlert()
+                                if error != nil {
+                                    if let error = error as? ValidationError {
+                                        self.presentValidationErrorAlert(from: error)
                                     } else {
-                                        self.performSegue(
-                                            withIdentifier: "unwindToMapViewController",
-                                            sender: self)
+                                        self.presentServerErrorAlert()
+                                    }
+                                } else {
+                                    User.login(username: email, password: password) { error in
+                                        if error != nil {
+                                            self.presentServerErrorAlert()
+                                        } else {
+                                            self.performSegue(
+                                                withIdentifier: "unwindToMapViewController",
+                                                sender: self)
+                                        }
                                     }
                                 }
-                            }
-                    }
+                        }
 
+                    } else {
+                        // somehow one or more of the user properties is nil?
+                        // this would never happen.
+                    }
                 } else {
-                    // somehow one or more of the user properties is nil?
-                    // this would never happen.
+                    presentSingleButtonAlert(
+                        title: "Password Too Short",
+                        message: "Make sure your password is at least 8 characters.")
                 }
             } else {
                 presentSingleButtonAlert(
-                    title: "Password Mismatch",
-                    message: "The password fields do not match.")
+                    title: "Incomplete Fields",
+                    message: "Please complete all fields before proceeding.")
             }
         } else {
             presentSingleButtonAlert(
-                title: "Incomplete fields",
-                message: "Please complete all fields before proceeding.")
+                title: "Password Mismatch",
+                message: "The password fields do not match.")
         }
     }
 }
