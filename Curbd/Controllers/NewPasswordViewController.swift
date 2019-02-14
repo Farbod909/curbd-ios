50eb7017d752ad6a22853e178f623679543786e8
rvdang <rvdang98@gmail.com>
2019-02-14 09:41:45 -0800

    Add newPasswordViewController

---
+++ Curbd/Controllers/NewPasswordViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NewPasswordViewController.swift
+//  Curbd
+//
+//  Created by Ryan Dang on 2/14/19.
+//  Copyright © 2019 Farbod Rafezy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewPasswordViewController: UIViewController {
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordConfirmationTextField: UITextField!
+    
+    func initializeAppearanceSettings() {
+        newPasswordTextField.underlined()
+        newPasswordConfirmationTextField.underlined()
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        initializeAppearanceSettings()
+    }
+    
+    @IBAction func resetPasswordButtonClick(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func closeButtonClick(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
