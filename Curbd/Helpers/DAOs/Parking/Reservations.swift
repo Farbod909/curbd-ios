29ed76317ef582956a4b04f7a4460e850df2826f
rvdang <rvdang98@gmail.com>
2019-03-18 23:55:11 -0700

    feat(DAO): Add Parking DAOs

---
+++ Curbd/Helpers/DAOs/Parking/Reservations.swift
@@ -0,0 +1,42 @@
+//
+//  Reservations.swift
+//  Curbd
+//
+//  Created by Ryan Dang on 3/18/19.
+//  Copyright © 2019 Farbod Rafezy. All rights reserved.
+//
+
+import Foundation
+
+enum ReservationRequest: String {
+    // literal
+    case get = ""
+    
+    // id
+    case id = "/"
+    case report = "/report/"
+    case cancel = "/cancel/"
+}
+
+class ReservationTable {
+    let url = baseURL + DatabaseUrl.reservations.rawValue
+    let request: ReservationRequest
+    var id = -1
+    init(request: ReservationRequest, id: Int?) {
+        self.request = request
+        if let id = id {
+            self.id = id
+        }
+    }
+    
+    var path: String {
+        get {
+            switch(self.request) {
+            case .get:
+                return self.url + request.rawValue
+            default:
+                return self.url + "\(self.id)" + request.rawValue
+            }
+        }
+    }
+}
